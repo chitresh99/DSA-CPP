caa22ad451f8728dd92ff269e08252893adc7849
chitresh99 <chitreshpoojary990@gmail.com>
2024-03-27 02:27:01 +0530

    created a queue stl file

---
+++ Queue/Stl/queue.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    queue <int> q;
+
+    q.push(15);
+    q.push(16);
+
+
+    cout<<"size of queue"<<q.size()<<endl;
+
+    return 0;
+
+}
