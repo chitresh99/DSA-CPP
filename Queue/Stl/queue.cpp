1bb43a80b7e3d5037706fa5bb8a2a765bed7f958
chitresh99 <chitreshpoojary990@gmail.com>
2024-03-27 20:04:00 +0530

    queue stl implemented

--- Queue/Stl/queue.cpp
+++ Queue/Stl/queue.cpp
@@ -7,10 +7,29 @@ int main()
     queue <int> q;
 
     q.push(15);
+    
+    cout<<"front of q is :"<<q.front()<<endl;
+
     q.push(16);
+    q.push(17);
+
+    cout<<"rear of q is :"<<q.back()<<endl;
+    
+    
+    cout<<"size of queue is :"<<q.size()<<endl;
+
+    q.pop();
 
 
-    cout<<"size of queue"<<q.size()<<endl;
+    cout<<"size of queue is:"<<q.size()<<endl;
+    
+    if(q.empty()){
+      
+     cout<<"queue is empty"<<endl;
+
+    }else{
+        cout<<"queue is not empty"<<endl;
+    }
 
     return 0;
 
