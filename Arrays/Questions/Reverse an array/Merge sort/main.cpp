26b0dbb7f67bd8bd97e5235e70f93ca619b857e4
chitresh99 <chitreshpoojary990@gmail.com>
2024-04-14 18:10:01 +0530

    fixing errors in merge sort

--- Arrays/Questions/Reverse an array/Merge sort/main.cpp
+++ Arrays/Questions/Reverse an array/Merge sort/main.cpp
@@ -53,7 +53,7 @@ int main()
     int arr1[5] = {1, 3, 5, 7, 9};
     int arr2[3] = {2, 4, 6};
 
-    int arr3[8];
+    int arr3[8] = {0};
 
     merge(arr1, 5, arr2, 3, arr3); // array and their sizes
     print(arr3, 8);
