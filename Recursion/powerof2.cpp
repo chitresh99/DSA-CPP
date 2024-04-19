1953b05e6ca5758d1b326f968aefc21bc93b99eb
chitresh99 <chitreshpoojary990@gmail.com>
2024-04-19 16:18:22 +0530

    power of 2 implemented

---
+++ Recursion/powerof2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int power(int n){
+    if(n==0) //base case
+       return 1;
+    int smallprob = power(n-1);
+    int bigprob = 2 * smallprob;
+
+    return bigprob;    
+}
+
+
+int main (){
+    int n;
+    cin >> n;
+    
+    int ans = power(n);
+    cout << ans <<endl;
+    return 0;
+}
