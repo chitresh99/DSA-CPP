83bbb71114da6f0d7eba5fc94b15d1941fd78bb1
chitresh99 <chitreshpoojary990@gmail.com>
2024-04-19 16:03:42 +0530

    made some changes to the recursion code

--- Recursion/factorial.cpp
+++ Recursion/factorial.cpp
@@ -5,21 +5,30 @@ int factorial(int n)
 {
     // base case
 
+    // if (n == 0)
+    // {
+    //     return 1; // writing return in base case is mandatory
+    // }
+    // else
+    // {
+    //      int smallint = factorial(n-1);
+    //     //  int bigint = n * smallint; 
+
+    //     //  return bigint;  
+
+    //     //optimised approach
+
+    //     return n * smallint;    
+    // }
+
     if (n == 0)
-    {
-        return 1; // writing return in base case is mandatory
-    }
-    else
-    {
-         int smallint = factorial(n-1);
-        //  int bigint = n * smallint; 
+    return 1;
+    
+    int smallint = factorial(n-1);
+    int bigint = n * smallint; 
 
-        //  return bigint;  
-
-        //optimised approach
-
-        return n * smallint;    
-    }
+    return bigint;
+    
 }
 
 int main()
