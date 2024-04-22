3e6855f15781fc296656de4bf6c997021c8a0b19
chitresh99 <chitreshpoojary990@gmail.com>
2024-04-22 22:22:04 +0530

    implemented say stairs

---
+++ Recursion/Medium Level Problems/Say Stairs/staystairs.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void saydigit(int n, string arr[]){
+     
+     if(n==0)
+       return ;
+
+    int digit = n % 10;
+    
+    n = n / 10;
+    
+    
+
+    //recursive call
+    saydigit(n,arr);
+
+    cout << arr[digit]<< " "; //storing the digit or updating it
+    
+
+
+}
+
+
+int main () {
+    
+    string arr[10] = {
+        "Zero","One","Two","Three",
+        "Four","Five","Six",
+        "Seven","Eight","Nine"
+    };
+
+    int n;
+    cin>> n;
+    cout << endl << endl << endl;
+    saydigit(n,arr);
+    cout << endl << endl << endl;
+    return 0;
+}
