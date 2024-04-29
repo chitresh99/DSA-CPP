2ee2c411b3c02fb589b6420e984a933a01af9841
chitresh99 <chitreshpoojary990@gmail.com>
2024-04-29 22:54:09 +0530

    solved a code studios problem

---
+++ Recursion/Simple problems/Reverse a string/code studios/main.cpp
@@ -0,0 +1,26 @@
+//link :- https://www.naukri.com/code360/problems/reverse-the-string_799927?leftPanelTab=0&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_13&leftPanelTabValue=SUBMISSION
+
+// #include <bits/stdc++.h> 
+
+// void reverse (string &str,int i,int j) {
+   
+//     //base case
+//     if(i>j){  //referring to index of i and j
+//          return;
+//     }
+    
+//     swap(str[i],str[j]);
+//     i++; //incrementing the pointer
+//     j--; //decrementing the pointer
+
+//     //recursive call
+
+//     reverse(str,i,j);
+// }
+
+
+// string reverseString(string str)
+// {
+// 	reverse(str,0,str.length()-1);
+// 	return str;
+// }
