d1c73383de26f6bb6f45874e09e886331486a4a0
chitresh99 <chitreshpoojary990@gmail.com>
2024-05-03 22:41:34 +0530

    revising oop in cpp0

---
+++ Basic Concepts/classandobjects.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+//creating a class
+class hero{
+     
+    //some properties for the class
+    // char name[100];
+    // int health;
+    // int level;
+
+    int value;
+};
+
+
+int main() {
+    
+
+    //creating objects from class
+
+    hero h1;
+    
+    //checking the size of the object
+    cout << "size : " <<sizeof(h1) <<endl; 
+}
