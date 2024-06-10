ffd81b4165c1faec0a3c4e71fa18fb36a69369e8
chitresh99 <chitreshpoojary990@gmail.com>
2024-06-10 09:55:51 +0530

    factorial problem solved

---
+++ Revision/Recursion/Factorial/factorial.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int recursion(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        int smallprob = recursion(n - 1);
+        int bigproblem = n * smallprob;
+        return bigproblem;
+    }
+}
+
+int main()
+{
+
+    int n;
+
+    cin >> n;
+
+    int ans = recursion(n);
+
+    cout << ans << endl;
+
+    return 0;
+}
