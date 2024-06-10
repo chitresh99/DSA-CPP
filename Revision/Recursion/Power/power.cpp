ae2d4ae5290db6c10657822d535e732dc07fe0c0
chitresh99 <chitreshpoojary990@gmail.com>
2024-06-10 10:06:10 +0530

    power raised to 2 problem solved

---
+++ Revision/Recursion/Power/power.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int power(int n)
+{
+
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        int small =power(n - 1);
+        int big = 2 * small;
+        return big;
+    }
+}
+
+int main()
+{
+    int n;
+    cout <<"Enter the power"<<endl;
+    cin >> n;
+
+    int answer = power(n);
+    
+
+    cout << "Power of 2 raised is "<<answer <<endl;
+    return 0;
+}
