4203caedf7830a7bf7fe151944fa5dd80dbb885d
chitresh99 <chitreshpoojary990@gmail.com>
2024-06-27 22:54:57 +0530

    linear search

---
+++ Strivers SDE sheet/Arrays/Linear Search/Code Ninjas/linear_search.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int linearSearch(int n, int num, vector<int> &arr)
+{
+    for(int i = 0; i < n ; i++){
+        if(arr[i] == num){
+            return i;
+        }
+    }
+    return -1;
+}
