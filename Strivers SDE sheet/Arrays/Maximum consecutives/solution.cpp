66be3cd103f73926b5ddfe3fc82718c3d3a15b43
chitresh99 <chitreshpoojary990@gmail.com>
2024-07-09 15:14:53 +0530

    minor fixes

--- Strivers SDE sheet/Arrays/Maximum consecutives/solution.cpp
+++ Strivers SDE sheet/Arrays/Maximum consecutives/solution.cpp
@@ -2,23 +2,32 @@
 #include <algorithm>
 using namespace std;
 
-int findconsecutives(int arr[] , int size){
-int maxi = 0; //maximum
-int cnt = 0; //counter
+int findconsecutives(int arr[], int size)
+{
+    int maxi = 0; // maximum
+    int cnt = 0;  // counter
 
-for(int i = 0; i < size ; i++){
-    if(i == 1){
-        cnt++;
-        maxi = max(maxi,cnt);
-
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == 1)
+        {
+            cnt++;
+            maxi = max(maxi, cnt);
+        }
+        else
+        {
+            cnt = 0;
+        }
     }
-}
+    return maxi;
 }
 
-int main () {
+int main()
+{
 
-    int arr[6] = {1,1,0,1,1,1};
+    int arr[6] = {1, 1, 0, 1, 1, 1};
     int size = 6;
 
-    int ans = findconsecutives(arr,6);
+    int ans = findconsecutives(arr, 6);
+    cout << ans << endl;
 }
